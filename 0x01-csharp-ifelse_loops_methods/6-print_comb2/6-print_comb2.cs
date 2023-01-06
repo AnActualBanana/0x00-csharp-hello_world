0d21ea0e538ac299d3fa7ed1f6cd8f5e9cefdebf
AnActualBanana <loganmcclain59@gmail.com>
2023-01-06 14:39:02 -0600

    task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+public static void Main(string[] args)
+{
+for (int i = 0; i <= 9; i++)
+{
+for (int j = 0; j <= 9; j++)
+{
+if (i != j)
+{
+Console.Write("{0}, {1}", i, j);
+Console.WriteLine();
+}
+}
+}
+}
+}
