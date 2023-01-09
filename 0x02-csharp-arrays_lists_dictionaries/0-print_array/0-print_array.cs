bf6eb45357319daa582b27f12cab0238a3382812
AnActualBanana <loganmcclain59@gmail.com>
2023-01-09 09:13:29 -0600

    rework, test

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,17 +1,25 @@
-public static int[] CreatePrint(int size) {
-    if (size < 0) {
-        System.out.println("Size cannot be negative");
-        return null;
+using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int[] newArray = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write("{0}", i);
+            if (i != size - 1)
+            {
+                Console.Write(" ");
+            }
+            newArray[i] = i;
+        }
+        Console.WriteLine();
+        return newArray;
     }
-    if (size == 0) {
-        System.out.println();
-        return new int[0];
-    }
-    int[] array = new int[size];
-    for (int i = 0; i < size; i++) {
-        array[i] = i;
-        System.out.print(array[i] + " ");
-    }
-    System.out.println();
-    return array;
 }
