133a9f09af68c209a1070fb62a2c62f48ab59969
AnActualBanana <loganmcclain59@gmail.com>
2023-01-20 12:35:57 -0600

    task 3

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Enemies
+{
+    ///<summary> Zombie Class. </summary>
+    public class Zombie
+    {
+        ///<summary> zombie's health. </summary>
+        private int health;
+        ///<summary> default constructor for zombie. </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        ///<summary> constructor for zombie with custom health value. </summary>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+            {
+                health = value;
+            }
+            else
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+        ///method that returns the value of health of the Zombie object. </summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
