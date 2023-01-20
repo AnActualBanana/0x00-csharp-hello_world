acb1d019faaafa2952d11d2a7538b7a851678422
AnActualBanana <loganmcclain59@gmail.com>
2023-01-20 12:22:21 -0600

    task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Enemies
+{
+    ///<summary> Zombie Class. </summary>
+    public class Zombie
+    {
+        ///<summary> zombie's health. </summary>
+        public int health;
+        ///<summary> constructor for zombie. </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
