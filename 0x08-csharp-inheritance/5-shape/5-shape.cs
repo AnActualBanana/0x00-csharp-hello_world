e6674e4aa4fa7e37f8b468a41d4d6c446a746eab
AnActualBanana <loganmcclain59@gmail.com>
2023-01-23 09:39:47 -0600

    task 5

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,12 @@
+using System;
+
+/// <summary>Shape Class</summary>
+class Shape
+{
+    /// <summary>Throws exception</summary>
+    public virtual int Area()
+    {
+        throw new System.NotImplementedException("Area() is not implemented");
+    }
+    
+}
