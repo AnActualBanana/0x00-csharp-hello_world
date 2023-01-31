d7e528d6661d615c9673c6b83d5a7e5129d6c8f7
AnActualBanana <loganmcclain59@gmail.com>
2023-01-31 14:04:09 -0600

    task 18

---
+++ 0x00-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,32 @@
+using System;
+
+class MatrixMath
+{
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        int rows1 = matrix1.GetLength(0);
+        int columns1 = matrix1.GetLength(1);
+        int rows2 = matrix2.GetLength(0);
+        int columns2 = matrix2.GetLength(1);
+
+        if (columns1 != rows2)
+        {
+            return new double[,] { { -1 } };
+        }
+
+        double[,] result = new double[rows1, columns2];
+
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < columns2; j++)
+            {
+                for (int k = 0; k < columns1; k++)
+                {
+                    result[i, j] += matrix1[i, k] * matrix2[k, j];
+                }
+            }
+        }
+
+        return result;
+    }
+}
