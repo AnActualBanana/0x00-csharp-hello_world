45ba76c5a44b3cc6117b76b406534339da147296
AnActualBanana <loganmcclain59@gmail.com>
2023-01-31 15:06:50 -0600

    XML

--- 0x01-csharp-generics/0-queue/queue.cs
+++ 0x01-csharp-generics/0-queue/queue.cs
@@ -1,87 +1,90 @@
 using System;
 
-public class Queue<T>
+namespace QueueImplementation
 {
-    private T[] items;
-    private int startIndex = 0;
-    private int endIndex = -1;
-
-    public Queue()
+    /// <summary>
+    /// A generic class for creating Queue data structure
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the queue</typeparam>
+    public class Queue<T>
     {
-        items = new T[0];
-    }
+        private T[] elements;
+        private int head;
+        private int tail;
+        private int count;
 
-    public void Enqueue(T item)
-    {
-        if (endIndex + 1 >= items.Length)
+        /// <summary>
+        /// Initializes a new instance of the Queue class that is empty and has the default initial capacity.
+        /// </summary>
+        public Queue()
         {
-            T[] newArray = new T[items.Length + 10];
-            for (int i = 0; i < items.Length; i++)
+            elements = new T[0];
+            head = 0;
+            tail = -1;
+            count = 0;
+        }
+
+        /// <summary>
+        /// Gets the number of elements contained in the queue.
+        /// </summary>
+        public int Count
+        {
+            get { return count; }
+        }
+
+        /// <summary>
+        /// Adds an object to the end of the queue.
+        /// </summary>
+        /// <param name="element">The object to add to the queue. The value can be null for reference types.</param>
+        public void Enqueue(T element)
+        {
+            if (count == elements.Length)
             {
-                newArray[i] = items[i];
+                T[] newElements = new T[elements.Length == 0 ? 4 : 2 * elements.Length];
+                Array.Copy(elements, head, newElements, 0, count);
+                elements = newElements;
+                head = 0;
+                tail = count - 1;
             }
-            items = newArray;
+            tail = (tail + 1) % elements.Length;
+            elements[tail] = element;
+            count++;
         }
 
-        items[++endIndex] = item;
-    }
-
-    public T Dequeue()
-    {
-        if (endIndex == -1)
+        /// <summary>
+        /// Removes and returns the object at the beginning of the queue.
+        /// </summary>
+        /// <returns>The object that is removed from the beginning of the queue</returns>
+        public T Dequeue()
         {
-            throw new InvalidOperationException("The queue is empty.");
+            if (count == 0)
+                throw new InvalidOperationException("The queue is empty");
+
+            T removed = elements[head];
+            head = (head + 1) % elements.Length;
+            count--;
+            return removed;
         }
 
-        T item = items[startIndex];
-        items[startIndex++] = default(T);
-
-        if (startIndex > endIndex)
+        /// <summary>
+        /// Returns the object at the beginning of the queue without removing it.
+        /// </summary>
+        /// <returns>The object at the beginning of the queue</returns>
+        public T Peek()
         {
-            startIndex = 0;
-            endIndex = -1;
+            if (count == 0)
+                throw new InvalidOperationException("The queue is empty");
+
+            return elements[head];
         }
 
-        return item;
-    }
-
-    public T Peek()
-    {
-        if (endIndex == -1)
+        /// <summary>
+        /// Check the type of the elements in the queue.
+        /// </summary>
+        /// <returns>The type of the elements in the queue</returns>
+        public Type CheckType()
         {
-            throw new InvalidOperationException("The queue is empty.");
+            return typeof(T);
         }
-
-        return items[startIndex];
-    }
-
-    public int Count
-    {
-        get
-        {
-            return endIndex - startIndex + 1;
-        }
-    }
-
-    public void Clear()
-    {
-        startIndex = 0;
-        endIndex = -1;
-    }
-
-    public T[] ToArray()
-    {
-        T[] array = new T[Count];
-        for (int i = 0; i < Count; i++)
-        {
-            array[i] = items[startIndex + i];
-        }
-
-        return array;
-    }
-
-    public string CheckType()
-    {
-        return typeof(T).ToString();
     }
 }
