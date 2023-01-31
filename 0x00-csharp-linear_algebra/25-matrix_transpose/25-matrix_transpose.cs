ccd13d7ae58528bf74f37e5c244433386babcacd
AnActualBanana <loganmcclain59@gmail.com>
2023-01-31 14:27:16 -0600

    task 25

---
+++ 0x00-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,17 @@
+using System;
+
+public static double[,] Transpose(double[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    double[,] transposedMatrix = new double[columns, rows];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            transposedMatrix[j, i] = matrix[i, j];
+        }
+    }
+    return transposedMatrix;
+}
