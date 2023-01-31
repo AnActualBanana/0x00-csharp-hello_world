f203bcf8990e78e81b2620c4844278c991d38a5e
AnActualBanana <loganmcclain59@gmail.com>
2023-01-31 09:39:36 -0600

    6

--- 0x00-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x00-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,23 +1,20 @@
 using System;
 
-namespace VectorMath
-{
-    public class Vector
+    public static class VectorMath
     {
         public static double[] Add(double[] vector1, double[] vector2)
         {
-            if (vector1.Length != 2 && vector1.Length != 3)
+            if (vector1.Length != vector2.Length || (vector1.Length != 2 && vector1.Length != 3))
+            {
                 return new double[] { -1 };
-            if (vector2.Length != 2 && vector2.Length != 3)
-                return new double[] { -1 };
-            if (vector1.Length != vector2.Length)
-                return new double[] { -1 };
+            }
 
             double[] result = new double[vector1.Length];
             for (int i = 0; i < vector1.Length; i++)
+            {
                 result[i] = vector1[i] + vector2[i];
+            }
 
             return result;
         }
     }
-}
