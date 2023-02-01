a51a8cdb64e30601bb7f97ebf2b100b716268cfc
AnActualBanana <loganmcclain59@gmail.com>
2023-02-01 16:09:01 -0600

    tasjk 4

--- 0x01-csharp-generics/4-print/queue.cs
+++ 0x01-csharp-generics/4-print/queue.cs
@@ -1,86 +1,99 @@
 using System;
 
-public class Queue<T>
+///<summary>Class for que opperations</summary>
+class Queue<T>
 {
-    private class Node
-    {
-        public T value;
-        public Node next;
+	//Setting node for queues
+	public class Node
+	{
+		public T value = default(T);
+		public Node next = null;
 
-        public Node(T value)
-        {
-            this.value = value;
-            this.next = null;
-        }
-    }
+		public Node(T input)
+		{
+			value = input;
+		}
+	}
 
-    private Node head;
-    private Node tail;
-    private int count;
+	public Node head;
+	public Node tail;
+	public int count;
 
-    public Queue()
-    {
-        head = null;
-        tail = null;
-        count = 0;
-    }
+	///<summary>Add new node at the end</summary>
+	public void Enqueue(T value)
+	{
+		Node node = new Node(value);
+		if (head == null)
+		{
+			head = node;
+			tail = node;
+		}
+		else
+		{
+			tail.next = node;
+			tail = node;
+		}
+		count++;
+	}
 
-    public void Enqueue(T value)
-    {
-        Node node = new Node(value);
-        if (head == null)
-        {
-            head = node;
-            tail = node;
-        }
-        else
-        {
-            tail.next = node;
-            tail = node;
-        }
-        count++;
-    }
+	///<summary>Add deleting the last node</summary>
+	public T Dequeue()
+	{
+		if (head == null)
+		{
+			Console.WriteLine("Queue is empty");
+			return default(T);
+		}
+		else
+		{
+			T value = head.value;
+			head = head.next;
+			count--;
+			return value;
+		}
+	}
 
-    public int Count()
-    {
-        return count;
-    }
+	///<summary>Return the value of first node</summary>
+	public T Peek()
+	{
+		if (head == null)
+		{
+			Console.WriteLine("Queue is empty");
+			return default(T);
+		}
+		else
+		{
+			return head.value;
+		}
+	}
 
-    public T Dequeue()
-    {
-        if (head == null)
-        {
-            Console.WriteLine("Queue is empty");
-            return default(T);
-        }
-        Node node = head;
-        head = head.next;
-        count--;
-        return node.value;
-    }
+	///<summary>Print all nodes</summary>
+	public void Print()
+	{
+		if (head == null)
+		{
+			Console.WriteLine("Queue is empty");
+		}
+		else
+		{
+			while (head != null)
+			{
+				Console.WriteLine(head.value);
+				head = head.next;
+			}
+		}
+	}
 
-    public T Peek()
-    {
-        if (head == null)
-        {
-            Console.WriteLine("Queue is empty");
-            return default(T);
-        }
-        return head.value;
-    }
+	///<summary>Add new node at the end</summary>
+	public int Count()
+	{
+		return count;
+	}
 
-    public void Print()
-    {
-        if (head == null)
-        {
-            Console.WriteLine("Queue is empty");
-            return;
-        }
-        Node current = head;
-        while (current != null)
-        {
-            Console.WriteLine(current.value);
-            current = current.next;
-        }
-    }
+	///<summary>Check if its a queue type</summary>
+	///<return>returns a Queue type</return>
+	public Type CheckType()
+	{
+		return typeof(T);
+	}
 }
