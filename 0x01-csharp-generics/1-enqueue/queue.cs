f3891312db2400b93a99cc3d800d4de10bb27300
AnActualBanana <loganmcclain59@gmail.com>
2023-02-01 13:57:34 -0600

    Task 1

---
+++ 0x01-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,51 @@
+using System;
+
+namespace QueueProject
+{
+    public class Queue<T>
+    {
+        private class Node
+        {
+            public T value;
+            public Node next;
+
+            public Node(T value)
+            {
+                this.value = value;
+                next = null;
+            }
+        }
+
+        private Node head;
+        private Node tail;
+        private int count;
+
+        public Queue()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public void Enqueue(T value)
+        {
+            Node newNode = new Node(value);
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            count++;
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+    }
+}
