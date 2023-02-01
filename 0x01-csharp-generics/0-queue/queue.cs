0aa53ea7f3d7db81461e98f7177086c72cbf6103
AnActualBanana <loganmcclain59@gmail.com>
2023-02-01 16:08:03 -0600

    TASK

--- 0x01-csharp-generics/0-queue/queue.cs
+++ 0x01-csharp-generics/0-queue/queue.cs
@@ -1,17 +1,14 @@
 using System;
 
-/// <summary>
-/// Queue&lt;T&gt; class implements a generic queue data structure.
-/// </summary>
-/// <typeparam name="T">The type of elements in the queue.</typeparam>
-public class Queue<T>
-{
-    /// <summary>
-    /// Returns the type of elements in the queue.
-    /// </summary>
-    /// <returns>The type of elements in the queue.</returns>
-    public Type CheckType()
+   /// <summary>MAkes new class</summary>
+   /// <typeparam name="T"></typeparam>
+    class Queue<T> 
     {
-        return typeof(T);
+      /// <summary>checks type</summary>
+      /// <returns>typeof</returns>
+      public Type CheckType()
+      {
+         return typeof(T);
+      }
+       
     }
-}
